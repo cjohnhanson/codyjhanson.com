c4c754d3cf49d0f5065871a76156bfae788c0ae0
Cody Hanson <cody@codyjhanson.com>
2022-04-28 17:05:43 -0500

    rewrote

--- codyjhanson.com/content/posts/dataops.md
+++
@@ -1,6 +0,0 @@
----
-title: "Dataops"
-date: 2019-10-17T11:04:00-05:00
-draft: true
----
-
---
+++ codyjhanson/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./layouts/*.html", "./layouts/**/*.html"],
+  theme: {
+    fontFamily: {
+      sans: "JetBrains Mono",
+    },
+    extend: {
+	colors: {
+	    "theme-blue": "#264653",
+	    "theme-green": "#2A9D8F",
+	    "theme-yellow":"#E9C46A",
+	    "theme-orange":"#F4A261",
+	    "theme-red":"#E76F51"
+	}
+    },
+  },
+  plugins: [],
+};
